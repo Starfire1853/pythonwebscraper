9e984fc31c6823d5d2c9e6a752f6843a8de6d46a
Kai Morita-McVey <kai.moritamcvey@gmail.com>
2018-12-13 14:46:46 -0800

    Added requests and bs4

--- scraper2.py
+++ scraper2.py
@@ -3,13 +3,24 @@ from bs4 import BeautifulSoup
 import variables
 import twilio
 from twilio.rest import Client
+import requests
 
-client = Client(variables.account_sid, variables.auth_token)
 
-message = client.messages.create(
-    body="Join Earth's mightiest heroes. Like Kevin Bacon.",
-    from_=variables.twilio_phone_number,
-    to=variables.my_phone_number,
-)
+req = requests.get(variables.url)
 
-print(message.sid)
+
+soup = BeautifulSoup(req.content, 'html.parser')
+
+print(soup.prettify())
+
+print("-------------------------------")
+
+print(soup.find_all("p"))
+# client = Client(variables.account_sid, variables.auth_token)
+# message = client.messages.create(
+#     body="Join Earth's mightiest heroes. Like Kevin Bacon.",
+#     from_=variables.twilio_phone_number,
+#     to=variables.my_phone_number,
+# )
+
+# print(message.sid)
