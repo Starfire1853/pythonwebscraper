1233f87f0373fa0b38cf4a5f571c3d35c6ddb1c6
Kai Morita-McVey <kai.moritamcvey@gmail.com>
2019-01-02 13:15:06 -0800

    removed excess code and worked on README

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
-pythonwebscraper
+# pythonwebscraper
+
+This is a webscraper with the purpose of texting the most recently posted workout on [https://www.crossfitmagnus.com/workouts/](https://www.crossfitmagnus.com/workouts/ "Workouts - Crossfit Magnus").
+This program used BeatutifulSoup4 and html2text to parse the html and twillio to message the text.
+
+To run this program you need a file called `variables.py`, it must contain these variables:
+
+```python
+url = 'https://www.crossfitmagnus.com/workouts/'
+account_sid = 'xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx'
+auth_token = 'xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx'
+twilio_phone_number = '+xxxxxxxxxxx'
+my_phone_number = '+1-xxx-xxx-xxxx'
+```
--- scraper2.py
+++ scraper2.py
@@ -8,47 +8,25 @@ import html2text
 
 req = requests.get(variables.url)
 soup = BeautifulSoup(req.content, 'html.parser')
+
+
+def texter(text):
+    client = Client(variables.account_sid, variables.auth_token)
+    message = client.messages.create(
+        body=text,
+        from_=variables.twilio_phone_number,
+        to=variables.my_phone_number,
+    )
+    print(message.sid)
+
+
 workoutblocks = soup.find("div", {"class": "e-content"}).findChildren("p")
 print(workoutblocks)
-
+print("\n \n")
 
 newtext = str(workoutblocks)
-# print(newtext.replace("<br/>", "\n"))
-print("-------------------------------")
-
 text = html2text.html2text(newtext.replace("[", "").replace("]", ""))
-print(text)
-print("-------------------------------")
 text = text.replace("_", "")
 print(text)
 
-client = Client(variables.account_sid, variables.auth_token)
-message = client.messages.create(
-    # body="Join Earth's mightiest heroes. Like Kevin Bacon.",
-    body=(" \n" + text),
-    from_=variables.twilio_phone_number,
-    to=variables.my_phone_number,
-)
-print(message.sid)
-# for div_tag in workoutblocks:
-
-#     workouttext = div_tag.text
-#     # print(div_tag.text, div_tag.next_sibling)
-#     print(workouttext)
-#     print("-------------------------------")
-#     print(workouttext.replace("A", "A\n"))
-#     print("-------------------------------")
-#     ftext = workouttext.replace(" B.", "\n B.")
-#     ftext2 = ftext.replace(" C.", "\n C.")
-
-#     client = Client(variables.account_sid, variables.auth_token)
-#     message = client.messages.create(
-#         # body="Join Earth's mightiest heroes. Like Kevin Bacon.",
-#         body=ftext2,
-#         from_=variables.twilio_phone_number,
-#         to=variables.my_phone_number,
-#     )
-
-
-# print("-------------------------------")
-# print(message.sid)
+texter(text)
