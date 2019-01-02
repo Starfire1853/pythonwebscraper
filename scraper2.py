d8e43251c3919302ad15d60bb42adb1c1d44a879
Kai Morita-McVey <kai.moritamcvey@gmail.com>
2019-01-02 12:52:41 -0800

    fixed formating so paragraphs show up correctly without any tags showing

--- scraper2.py
+++ scraper2.py
@@ -4,31 +4,51 @@ import variables
 import twilio
 from twilio.rest import Client
 import requests
-
+import html2text
 
 req = requests.get(variables.url)
-
-
 soup = BeautifulSoup(req.content, 'html.parser')
+workoutblocks = soup.find("div", {"class": "e-content"}).findChildren("p")
+print(workoutblocks)
 
 
-workoutblocks = soup.find("div", {"class": "e-content"})
-# print(workoutblocks)
-print("-------------------------------")
-for div_tag in workoutblocks:
-
-    workouttext = div_tag.text, div_tag.next_sibling
-    # print(div_tag.text, div_tag.next_sibling)
-    print(workouttext)
-    client = Client(variables.account_sid, variables.auth_token)
-    message = client.messages.create(
-        # body="Join Earth's mightiest heroes. Like Kevin Bacon.",
-        body=workouttext,
-        from_=variables.twilio_phone_number,
-        to=variables.my_phone_number,
-    )
-
+newtext = str(workoutblocks)
+# print(newtext.replace("<br/>", "\n"))
 print("-------------------------------")
 
+text = html2text.html2text(newtext.replace("[", "").replace("]", ""))
+print(text)
+print("-------------------------------")
+text = text.replace("_", "")
+print(text)
 
+client = Client(variables.account_sid, variables.auth_token)
+message = client.messages.create(
+    # body="Join Earth's mightiest heroes. Like Kevin Bacon.",
+    body=(" \n" + text),
+    from_=variables.twilio_phone_number,
+    to=variables.my_phone_number,
+)
 print(message.sid)
+# for div_tag in workoutblocks:
+
+#     workouttext = div_tag.text
+#     # print(div_tag.text, div_tag.next_sibling)
+#     print(workouttext)
+#     print("-------------------------------")
+#     print(workouttext.replace("A", "A\n"))
+#     print("-------------------------------")
+#     ftext = workouttext.replace(" B.", "\n B.")
+#     ftext2 = ftext.replace(" C.", "\n C.")
+
+#     client = Client(variables.account_sid, variables.auth_token)
+#     message = client.messages.create(
+#         # body="Join Earth's mightiest heroes. Like Kevin Bacon.",
+#         body=ftext2,
+#         from_=variables.twilio_phone_number,
+#         to=variables.my_phone_number,
+#     )
+
+
+# print("-------------------------------")
+# print(message.sid)
