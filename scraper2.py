cd0fae29759b2f0d592a7752f5b86c75958fb60f
Kai Morita-McVey <kai.moritamcvey@gmail.com>
2019-01-02 10:37:36 -0800

    added texting functionality of most recent workout

--- scraper2.py
+++ scraper2.py
@@ -11,24 +11,24 @@ req = requests.get(variables.url)
 
 soup = BeautifulSoup(req.content, 'html.parser')
 
-# print(soup.prettify())
-
-print("-------------------------------")
-
-# print(soup.find_all("p"))
-
 
 workoutblocks = soup.find("div", {"class": "e-content"})
-print(workoutblocks)
+# print(workoutblocks)
 print("-------------------------------")
 for div_tag in workoutblocks:
-    print(div_tag.text, div_tag.next_sibling)
-print("-------------------------------")
-# client = Client(variables.account_sid, variables.auth_token)
-# message = client.messages.create(
-#     body="Join Earth's mightiest heroes. Like Kevin Bacon.",
-#     from_=variables.twilio_phone_number,
-#     to=variables.my_phone_number,
-# )
 
-# print(message.sid)
+    workouttext = div_tag.text, div_tag.next_sibling
+    # print(div_tag.text, div_tag.next_sibling)
+    print(workouttext)
+    client = Client(variables.account_sid, variables.auth_token)
+    message = client.messages.create(
+        # body="Join Earth's mightiest heroes. Like Kevin Bacon.",
+        body=workouttext,
+        from_=variables.twilio_phone_number,
+        to=variables.my_phone_number,
+    )
+
+print("-------------------------------")
+
+
+print(message.sid)
